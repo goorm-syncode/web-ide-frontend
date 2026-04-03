6da1cc28de1f13e05e0e719e0d3892bd567680c0
LynnYi2025 <a01033504631@gmail.com>
2026-04-03 09:25:59 +0900

    Revert "feat: add DevProblemListPage and ProblemList component with routing c…"

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -5,7 +5,6 @@ import DevMessageBoxPage from '../pages/dev/DevMessageBoxPage';
 import DevGnbPage from '../pages/dev/DevGnbPage';
 import DevFooterPage from '../pages/dev/DevFooterPage';
 import DevProblemCardPage from '../pages/dev/DevProblemCardPage';
-import DevProblemListPage from '../pages/dev/DevProblemListPage';
 
 const router = createBrowserRouter([
   {
@@ -27,14 +26,8 @@ const router = createBrowserRouter([
   {
     path: '/dev/footer',
     element: <DevFooterPage />,
-  },
-  {
     path: '/dev/problem-card',
     element: <DevProblemCardPage />,
-  },
-  {
-    path: '/dev/problem-list',
-    element: <DevProblemListPage />,
   }
 ]);
 
