b2c9995ad32f0db10ff53b708b9449b1dccf583c
bil-413@hanmail.net <bil-413@hanmail.net>
2026-04-10 16:28:35 +0900

    feat: add ProgressBanner component with skeleton loading state and global base styles

--- src/components/common/ProgressBanner.jsx
+++ src/components/common/ProgressBanner.jsx
@@ -30,7 +30,7 @@ const ProgressBanner = ({ progress = 0, onContinue, loading = false, className =
         <span className="progress-text">{progress}%</span>
       </div>
       <button className="progress-continue-btn" onClick={onContinue}>
-        Continue Learning
+        이어하기
         <svg 
           xmlns="http://www.w3.org/2000/svg" 
           width="18" 
